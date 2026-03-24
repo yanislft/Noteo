9de97435e651ea9fe661387313bd1d18bd95c37d
Yanis Laftimi <ylaftimi@icloud.com>
2026-03-24 23:09:38 +0100

    initialisation authentification frontend

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,26 @@
+import api from './axios';
+import type { User } from '../types';
+
+export const register = async (data: {
+  name: string;
+  firstname: string;
+  email: string;
+  password: string;
+}) => {
+  const response = await api.post<{ token: string; user: User }>('/auth/register', data);
+  return response.data;
+};
+
+export const login = async (data: { email: string; password: string }) => {
+  const response = await api.post<{ token: string; user: User }>('/auth/login', data);
+  return response.data;
+};
+
+export const logout = async () => {
+  await api.post('/auth/logout');
+};
+
+export const me = async () => {
+  const response = await api.get<User>('/auth/me');
+  return response.data;
+};
