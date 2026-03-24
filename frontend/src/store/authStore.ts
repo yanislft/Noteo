10a11298620b1ec4d11a8ade7b3da7c187863ff9
Yanis Laftimi <ylaftimi@icloud.com>
2026-03-24 23:13:59 +0100

    généralisation de l'authentification

---
+++ frontend/src/store/authStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+import type { User } from '../types';
+
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  setAuth: (user: User, token: string) => void;
+  clearAuth: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  token: localStorage.getItem('token'),
+  setAuth: (user, token) => {
+    localStorage.setItem('token', token);
+    set({ user, token });
+  },
+  clearAuth: () => {
+    localStorage.removeItem('token');
+    set({ user: null, token: null });
+  },
+}));
